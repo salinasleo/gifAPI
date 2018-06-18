2fa3599cfddf195097de6ffd97fc8d414373afcb
Leo Salinas <salinasleo@gmail.com>
2018-06-18 19:36:47 -0400

    added stop and play functionality to gif images, and made few topic form work press enter in addition to on click button

--- assets/js/jslogic.js
+++ assets/js/jslogic.js
@@ -5,17 +5,16 @@ var searchkey;
 var apiKEY = 'Gm3tQOLOqdjDefZBvFMEykpANaK1W3Zz';
 
 function makebuttons() {
-$("#buttonSection").html("");
-for (i = 0; i < subjects.length; i++) {
-       // $("#buttonSection").append('<button  CLASS="GifButton">' + subjects[i] + '</button>');
-    $("#buttonSection").append("<a href='#' class ='GifButton btn btn-success' id='" + subjects[i] + "'>"
-        + subjects[i] + "</a>");
-    $("#buttonSection").append( "&nbsp;&nbsp;");
-       
-}
+    $("#buttonSection").html("");
+    for (i = 0; i < subjects.length; i++) {
+        $("#buttonSection").append("<a href='#' class ='GifButton btn btn-success' id='" + subjects[i] + "'>"
+            + subjects[i] + "</a>");
+        $("#buttonSection").append("&nbsp;&nbsp;");
+
+    }
 }
 
-    makebuttons(); 
+makebuttons();
 
 $(document).on("click", '.GifButton', setTopic);
 $(document).on("click", '.GifButtonForm', setTopicForm);
@@ -24,18 +23,15 @@ function setTopicForm() {
     searchkey = $("#addTopic").val().trim();
     console.log(searchkey);
     if (searchkey !== "") {
-    subjects.push(searchkey);
-    makebuttons();
-    // $("#buttonSection").append("<a href='#' class ='GifButton btn btn-success' id='" + searchkey + "'>"
-    // + searchkey + "</a>");
-    // $("#buttonSection").append( "&nbsp;&nbsp;");
-    getGIFs();   
+        subjects.push(searchkey);
+        makebuttons();
+        getGIFs();
     }
     else alert("please enter a valid new topic")
 }
 
 function setTopic() {
-    searchkey=this.text;
+    searchkey = this.text;
     getGIFs();
 }
 
@@ -60,15 +56,20 @@ function getGIFs() {
             if (results[i].rating !== "r") {
                 var rating = results[i].rating;
                 var gifs = $("<div>");
-                
+
                 var p = $("<p>").text("Rated: " + rating);
 
                 var imagehere = $("<img>");
 
                 imagehere.attr("src", results[i].images.fixed_height.url);
+                imagehere.attr("data-state", "animate");
+                imagehere.attr("id", "gif");
+                imagehere.attr("data-still",results[i].images.fixed_height_still.url );
+                imagehere.attr("data-animate",results[i].images.fixed_height.url );
 
                 gifs.append(p);
                 gifs.append(imagehere);
+                
 
                 $("#gifContainer").prepend(imagehere);
                 $("#gifContainer").prepend(p);
@@ -76,3 +77,27 @@ function getGIFs() {
         }
     });
 }
+
+// start and stop gifs
+ 
+$(document).on("click", "#gif", function() {
+    var state = $(this).attr("data-state");
+    console.log(state);
+    var dataAnimate = $(this).attr("data-animate");
+    var dataStill = $(this).attr("data-still");
+
+        if (state === "still") {
+            $(this).attr("src", dataAnimate);
+            $(this).attr("data-state", "animate");
+        }
+        else {
+            $(this).attr("src", dataStill);
+            $(this).attr("data-state", "still")
+        }
+    });
+
+    document.getElementById('addTopic').addEventListener('keypress', function(event) {
+        if (event.keyCode == 13) {
+            setTopicForm();
+        }
+    });
